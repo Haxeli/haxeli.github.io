68d37d1f01fa8625cf05d866c485a3b7cdaa7339
Aleksi <79052109+Haxeli@users.noreply.github.com>
2024-01-08 18:45:55 +0200

    Modified CSS for mobile devices

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,10 @@
-import styles from './page.module.css'
+//import styles from './page.module.css'
 import Head from 'next/head'
 import ProfileSection from './components/ProfileSection'
 
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <main>
       <Head>
         <title>Aleksi Hannula - Portfolio</title>
       </Head>
