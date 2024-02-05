11a551676d473b127ab2dab1aac2b5d0886f5725
Aleksi <79052109+Haxeli@users.noreply.github.com>
2024-02-05 10:33:56 +0200

    Fixed tab title and CSS warnings

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,14 @@
 //import styles from './page.module.css'
-import Head from 'next/head'
 import ProfileSection from './components/ProfileSection'
+
+export const metadata = {
+    title: 'Aleksi Hannula - Portfolio',
+    description: 'Aleksi Hannula is a software developer from Finland. This is his W-I-P portfolio.',
+}
 
 export default function Home() {
   return (
     <main>
-      <Head>
-        <title>Aleksi Hannula - Portfolio</title>
-      </Head>
       <ProfileSection />
     </main>
   )
